ccbf48d28075d9f2e0bf645453726e42edf1a192
zeroy <2020150384@qq.com>
2022-12-09 17:04:00 +0800

    Update

--- src/config/cache.go
+++ src/config/cache.go
@@ -2,38 +2,38 @@ package config
 
 // Cache is the configuration of the redis cache server.
 import (
-	"errors"
+    "errors"
 
-	"github.com/spf13/viper"
+    "github.com/spf13/viper"
 )
 
 type Cache struct {
-	Path string `mapstructure:"path" json:"path" yaml:"path"`
+    Path string `mapstructure:"path" json:"path" yaml:"path"`
 }
 
 var CacheConfig Cache
 
 func init() {
-	Register(&CacheConfig)
+    Register(&CacheConfig)
 }
 
 func (c *Cache) Save() error {
-	viper.Set("cache", c)
-	err := viper.WriteConfig()
-	if err != nil {
-		return err
-	}
-	return nil
+    viper.Set("cache", c)
+    err := viper.WriteConfig()
+    if err != nil {
+        return err
+    }
+    return nil
 }
 
 func (c *Cache) Load() error {
-	configReader := viper.Sub("cache")
-	if configReader == nil {
-		return errors.New("could not read cache config")
-	}
-	err := configReader.Unmarshal(&c)
-	if err != nil {
-		return err
-	}
-	return nil
+    configReader := viper.Sub("cache")
+    if configReader == nil {
+        return errors.New("could not read cache config")
+    }
+    err := configReader.Unmarshal(&c)
+    if err != nil {
+        return err
+    }
+    return nil
 }
